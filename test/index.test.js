3894c967c532855207ef3783d5deedd66fe31bb4
KevenMarioN <kevenmario.n.r@gmail.com>
2021-10-24 15:57:28 -0300

    feat: Teste deveria retorna o valor passado por parâmetro

--- test/index.test.js
+++ test/index.test.js
@@ -3,5 +3,6 @@ const fizzBuzz = require('../src/index');
 describe('app',() => {
   it('should process integer',() =>{
     expect(fizzBuzz.processInteger(1)).toBe(1);
+    expect(fizzBuzz.processInteger(2)).toBe(2);
   });
 });
