13c4b25abf743b4e8b072f4bed50410aea39cd1b
KevenMarioN <kevenmario.n.r@gmail.com>
2021-10-24 16:00:17 -0300

    test: Ao passar o valor 3 deveria retorna Fizz

--- test/index.test.js
+++ test/index.test.js
@@ -4,5 +4,6 @@ describe('app',() => {
   it('should process integer',() =>{
     expect(fizzBuzz.processInteger(1)).toBe(1);
     expect(fizzBuzz.processInteger(2)).toBe(2);
+    expect(fizzBuzz.processInteger(3)).toBe('Fizz');
   });
 });
