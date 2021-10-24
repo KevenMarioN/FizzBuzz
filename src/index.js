bc102351ddbb56ab291659b45e811982dfcd984b
KevenMarioN <kevenmario.n.r@gmail.com>
2021-10-24 16:17:33 -0300

    feat: Implementação ao passar numeros que são multiplos de 3 e 5 o retorno será FizzBuzz

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ const BUZZ = "Buzz";
 
 module.exports = {
   processInteger : function (integer) {
+    if(integer % 3 === 0 && integer % 5 === 0)
+      return(FIZZ+BUZZ);
     if(integer % 3 === 0)
       return FIZZ;
     if(integer % 5 === 0)
       return BUZZ;
+
     return integer;
   }
 }
