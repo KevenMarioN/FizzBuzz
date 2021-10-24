3a8eab31f12e87cd3d93a8ea12bc59fd37fc49fd
KevenMarioN <kevenmario.n.r@gmail.com>
2021-10-24 16:13:04 -0300

    feat: Implementação de qualquer numero multiplo de 5 será retornado Buzz

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ module.exports = {
   processInteger : function (integer) {
     if(integer % 3 === 0)
       return FIZZ;
-    if(integer === 5)
+    if(integer % 5 === 0)
       return BUZZ;
     return integer;
   }
